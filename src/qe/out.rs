ddb78facd044b17189d948ac091fe9a8030e898d
Jusong Yu <jusong.yeu@gmail.com>
2025-01-10 12:20:52 +0100

    verbose parser

--- src/qe/out.rs
+++ src/qe/out.rs
@@ -1,31 +1,32 @@
 use nom::{
-    bytes::complete::{tag, take_until, take_until1, take_while1},
+    bytes::complete::{escaped, tag, take_until, take_until1, take_while1},
     character::{
-        complete::{newline, not_line_ending, space0},
+        complete::{char, newline, not_line_ending, space0},
         streaming::multispace0,
     },
-    combinator::{map, opt},
-    multi::many0,
+    combinator::{cut, map, opt},
+    error::{context, ContextError, ParseError, VerboseError},
+    multi::{many0, many_till},
     sequence::{delimited, preceded, terminated, tuple},
-    AsChar, IResult,
+    AsChar, IResult, Parser,
 };
 
-#[derive(Debug, Default)]
-struct TotalEnergies {
-    // For QE<6.5 there is also Harris_foulkes estimate energy, I didn't parse it.
-    total: Option<f64>,
-    // estimated scf accuracy
-    accuracy: Option<f64>,
-    // one-electron contribution
-    one_electron: Option<f64>,
-    // hartree contribution
-    hartree: Option<f64>,
-    // smearing
-    smearing: Option<f64>,
-    // XXX: ... more
-    // this may not exist
-    solvation: Option<f64>,
-}
+// #[derive(Debug, Default)]
+// struct TotalEnergies {
+//     // For QE<6.5 there is also Harris_foulkes estimate energy, I didn't parse it.
+//     total: Option<f64>,
+//     // estimated scf accuracy
+//     accuracy: Option<f64>,
+//     // one-electron contribution
+//     one_electron: Option<f64>,
+//     // hartree contribution
+//     hartree: Option<f64>,
+//     // smearing
+//     smearing: Option<f64>,
+//     // XXX: ... more
+//     // this may not exist
+//     solvation: Option<f64>,
+// }
 
 #[derive(Debug)]
 enum Block {
@@ -44,9 +45,9 @@ where
 
 /// ws remove white space (different from `wms` will not remove line break) before and after the inner parser
 /// It mostly used for identifier parser
-fn ws<'a, F, O>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O>
+fn ws<'a, F, O>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, VerboseError<&str>>
 where
-    F: FnMut(&'a str) -> IResult<&'a str, O>,
+    F: FnMut(&'a str) -> IResult<&'a str, O, VerboseError<&str>>,
 {
     delimited(space0, inner, space0)
 }
@@ -73,7 +74,7 @@ fn bare_ident(input: &str) -> IResult<&str, String> {
 
 // Parse a bare (unquoted) number
 // It is spaces separated and can contain: ".", "-", "+", "E" and 10 based digitals
-fn bare_number(input: &str) -> IResult<&str, String> {
+fn bare_number(input: &str) -> IResult<&str, String, VerboseError<&str>> {
     map(
         take_while1(|c: char| {
             c.is_dec_digit() || c == '-' || c == '+' || c == '.' || c == 'E' || c == 'e'
@@ -84,10 +85,10 @@ fn bare_number(input: &str) -> IResult<&str, String> {
 
 fn parse_key_value<'a>(
     delimiter: &'a str,
-) -> impl Fn(&'a str) -> IResult<&'a str, (String, String, Option<String>)> {
+) -> impl Fn(&'a str) -> IResult<&'a str, (String, String, Option<String>), VerboseError<&str>> {
     move |input: &'a str| {
-        let (input, key) = terminated(take_until(delimiter), tag(delimiter))(input)?;
-        let (input, value) = ws(bare_number)(input)?;
+        let (input, key) = context("parse aa", terminated(take_until(delimiter), tag(delimiter)))(input)?;
+        let (input, value) = context("number", ws(bare_number))(input)?;
 
         // Optionally parse a unit to the end of the line.
         let (input, unit_opt) = opt(preceded(space0, not_line_ending))(input)?;
@@ -99,48 +100,53 @@ fn parse_key_value<'a>(
 
 fn take_until_and_consume<'a>(
     term: &'a str,
-) -> impl Fn(&'a str) -> IResult<&'a str, (&'a str, &'a str)> {
+) -> impl Fn(&'a str) -> IResult<&'a str, (&'a str, &'a str), VerboseError<&str>> {
     move |input: &'a str| {
         let (input, (pre, term)) = tuple((take_until(term), tag(term)))(input)?;
         Ok((input, (pre, term)))
     }
 }
 
-fn parse_final_energies(input: &str) -> IResult<&str, Block> {
-    // initialize with default
-    let mut energies = TotalEnergies::default();
+type TotalEnergies = (Option<f64>, Option<f64>, Option<f64>);
 
+/// parsing total_energy, accuracy, smearing from following section
+/// !    total energy              =     -96.34322116 Ry
+///      estimated scf accuracy    <          2.4E-10 Ry
+///      smearing contrib. (-TS)   =       0.00000000 Ry
+///      internal energy E=F+TS    =     -96.34322116 Ry
+fn parse_tot_energy(input: &str) -> IResult<&str, TotalEnergies, VerboseError<&str>> {
     // The final energy section lead with a "!"
     let (input, _) = preceded(multispace0, ws(tag("!")))(input)?;
     let (input, (_, total_energy_str, _)) = terminated(parse_key_value(" = "), newline)(input)?;
     let (input, (_, energy_accuracy_str, _)) = terminated(parse_key_value(" < "), newline)(input)?;
     let (input, (_, smearing_energy_str, _)) = terminated(parse_key_value(" = "), newline)(input)?;
 
-    energies.total = Some(total_energy_str.parse::<f64>().unwrap());
-    energies.accuracy = Some(energy_accuracy_str.parse::<f64>().unwrap());
-    energies.smearing = Some(smearing_energy_str.parse::<f64>().unwrap());
+    let total = Some(total_energy_str.parse::<f64>().unwrap());
+    let accuracy = Some(energy_accuracy_str.parse::<f64>().unwrap());
+    let smearing = Some(smearing_energy_str.parse::<f64>().unwrap());
 
-    let (input, _) = take_until_and_consume("E is the sum of the following terms:")(input)?;
-    let (input, contribs) = many0(terminated(parse_key_value(" = "), newline))(input)?;
-
-    for (key, value, _) in contribs {
-        match key.as_str() {
-            "one-electron contribution" => {
-                let value = value.parse::<f64>().unwrap();
-                energies.one_electron = Some(value);
-            }
-            "hartree contribution" => {
-                let value = value.parse::<f64>().unwrap();
-                energies.hartree = Some(value);
-            }
-            _ => eprintln!("not ready"),
-        }
-    }
-
-    Ok((input, Block::TotalEnergies(energies)))
+    Ok((input, (total, accuracy, smearing)))
 }
 
-fn parse_final_mag(input: &str) -> IResult<&str, (String, String)> {
+fn parse_energy_components(input: &str) -> IResult<&str, Vec<(String, f64)>, VerboseError<&str>> {
+    let (input, _) = take_until_and_consume("E is the sum of the following terms:")(input)?;
+    let (input, (contribs, _)) =
+        many_till(terminated(parse_key_value(" = "), newline), tag("\n\n"))(input)?;
+
+    // discard the unit
+    // TODO: convert to eV
+    let contribs: Vec<_> = contribs
+        .iter()
+        .map(|(k, v, _u)| {
+            let v = v.parse::<f64>().unwrap();
+            (k.to_owned(), v)
+        })
+        .collect();
+
+    Ok((input, contribs))
+}
+
+fn parse_final_mag(input: &str) -> IResult<&str, (String, String), VerboseError<&str>> {
     let (input, _) = take_until("total magnetization")(input)?;
     let (input, (_, tot_mag, _)) = terminated(parse_key_value(" = "), newline)(input)?;
     let (input, (_, abs_mag, _)) = terminated(parse_key_value(" = "), newline)(input)?;
@@ -150,6 +156,8 @@ fn parse_final_mag(input: &str) -> IResult<&str, (String, String)> {
 
 #[cfg(test)]
 mod tests {
+    use nom::Finish;
+
     use super::*;
 
     #[test]
@@ -168,17 +176,17 @@ mod tests {
      total magnetization       =    -0.00 Bohr mag/cell
      absolute magnetization    =     0.00 Bohr mag/cell
 ";
-        let (_, got) = parse_final_energies(input).unwrap();
+        let (_, got) = parse_tot_energy(input).unwrap();
         dbg!(got);
 
-        let (_, got) = parse_final_mag(input).unwrap();
-        dbg!(got);
+        let e = context("energy component", parse_energy_components)(input).finish().err().unwrap();
+        dbg!(e);
     }
 
-    #[test]
-    fn final_a() {
-        let s = "2.4E-10";
-        let value: f64 = s.parse().expect("Failed to parse f64");
-        println!("{}", value); // prints: 2.4e-10
-    }
+    // #[test]
+    // fn final_a() {
+    //     let s = "2.4E-10";
+    //     let value: f64 = s.parse().expect("Failed to parse f64");
+    //     println!("{}", value); // prints: 2.4e-10
+    // }
 }
